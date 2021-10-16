0272423288d6eefdb877ebfe16e552a6e7394967
MathieuDeveloper <topfermathieu@yahoo.fr>
2021-10-16 08:47:58 +0200

    Initial commit

---
+++ Data/DataUtility.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.Data
+{
+    public static class DataUtility
+    {
+        public static string GetConnectionString(IConfiguration configuration)
+        {
+            //The default connection string will come from appSettings like usual
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            //It will be automatically overwriten if we are running on Heroku
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+            return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
+
+        }
+
+        public static string BuildConnectionString(string databaseUrl)
+        {
+            //Provide an object representation of a uniform ressource identifier (URI) and easy
+            var databaseUri = new Uri(databaseUrl);
+            var userInfo = databaseUri.UserInfo.Split(':');
+
+            //Provide a simple way to create and manage the connection strings used
+            var builder = new NpgsqlConnectionStringBuilder
+            {
+                Host = databaseUri.Host,
+                Port = databaseUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = databaseUri.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Prefer,
+                TrustServerCertificate = true
+
+            };
+            return builder.ToString();
+
+
+        }
+
+    }
+}
